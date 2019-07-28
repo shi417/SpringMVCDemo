9aa8eee67a6cba6984ece2548357b8e42ab3979f
shiyq <1136738187@qq.com>
2019-07-28 16:42:06 +0800

    Initial commit

---
+++ src/main/java/sysConfig/SpittrWebAppInitializer.java
@@ -0,0 +1,17 @@
+package sysConfig;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+    <head>
+        <title>Controller</title>title>
+    <head>
+    <body>
+        <h1>Shao Han SB</h1>
+    </body>
+<html>
